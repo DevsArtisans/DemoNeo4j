022308df8298879aec157271249cbf9cc8e1e166
SebastianCotrina16 <sebastiancotrina16@gmail.com>
2024-04-09 10:19:22 -0500

    GCSW-31 [@SebastianCotrina16] Teams Endpoint

---
+++ src/schemas/TeamSchema.ts
@@ -0,0 +1,35 @@
+import { createSchema } from "graphql-yoga"
+import TeamService from "../services/TeamService";
+
+const teamService = new TeamService()
+
+const TeamSchema = createSchema({
+  typeDefs: /* GraphQL */ `
+        type Team {
+            name: String!
+            description: String!
+            leader: String!
+        }
+
+        type Query {
+          getTeamByName(name: String!): Team
+        }
+        type Mutation{
+          createTeam(name:String!,description: String!, leader:String!): Team    
+        }
+      `,
+  resolvers: {
+    Query: {
+      getTeamByName: async(_,{name}) =>{
+        return await teamService.getTeamByName(name)
+      },
+    },
+    Mutation: {
+      createTeam: async(_,{name,description,leader}) =>{
+        return await teamService.createTeam({name,description,leader})
+      },
+    }
+  },
+});
+
+export default TeamSchema;
