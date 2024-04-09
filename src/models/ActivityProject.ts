50f4970cad3adfcc0f235c1bc275e1a3143e97c6
Diego Aranda Reyes <diegoandre0312@gmail.com>
2024-04-08 22:40:59 -0500

    GCSW-18 [@Andrear03] Activity Project Model

---
+++ src/models/ActivityProject.ts
@@ -0,0 +1,10 @@
+export interface ActivityProject {
+    name: string;
+    description: string;
+    status: string;
+    methodology: string;
+    creationDate: string;
+    startDate: string;
+    finalDate: string;
+ 
+  }
--- src/routes/MemberRoutes.ts
+++
@@ -1,9 +0,0 @@
-import type { Member } from "../models/Member";
-
-type Query = {
-    getMemberByEmail(email: String): Member
-}
-
-type Mutation = {
-    createMember(name:String!,email: String!, role:String!): Member
-}
