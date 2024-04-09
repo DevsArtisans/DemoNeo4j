426efdbf8873819eb998e4695a5e9357593ecab8
SebastianCotrina16 <sebastiancotrina16@gmail.com>
2024-04-09 12:12:18 -0500

    GCSW-34 [@SebastianCotrina16] Added to Schemas Server

--- src/schemas/MemberOfSchema.ts
+++ src/schemas/MemberOfSchema.ts
@@ -26,3 +26,4 @@ const MemberOfSchema = createSchema({
     },
   },
 });
+export default MemberOfSchema;
--- src/schemas/SchemasServer.ts
+++ src/schemas/SchemasServer.ts
@@ -1,13 +1,15 @@
 import { mergeSchemas } from 'graphql-yoga';
 import MemberSchema from './MemberSchema';
 import TeamSchema from './TeamSchema';
-import ActivityImplementationSchema from './ActivityImplementation';
+import ActivityImplementationSchema from './ActivityImplementationSchema';
+import MemberOfSchema from './MemberOfSchema';
 
 const schema = mergeSchemas({
     schemas: [
         MemberSchema,
         TeamSchema,
-        ActivityImplementationSchema
+        ActivityImplementationSchema,
+        MemberOfSchema,
     ]
 });
 
